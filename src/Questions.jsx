07d296796087ddacd52567ee3b19557870bd118a
Sulav <sulavshrestha023@gmail.com>
2022-05-19 17:20:21 +0545

    Functionality added to SingleQuestion component

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,12 +1,9 @@
 import questions from "./data";
+import SingleQuestion from "./SingleQuestion";
 const Questions = () => {
-  return (
-    <main>
-      {questions.map((question) => (
-        <p>{question.title}</p>
-      ))}
-    </main>
-  );
+  return questions.map((question) => (
+    <SingleQuestion question={question} key={question.id} />
+  ));
 };
 
 export default Questions;
---
+++ src/SingleQuestion.jsx
@@ -0,0 +1,5 @@
+const SingleQuestion = ({ question }) => {
+  return <p>{question.title}</p>;
+};
+
+export default SingleQuestion;
